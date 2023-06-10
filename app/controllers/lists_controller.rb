fcd778605893bedfe5cd55bbb86a581d25ee755c
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-06-10 01:36:45 +0000

    [Finish]2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,27 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
   end
 
-  def index
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+
+     end
+
+def index
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  #ストロングパロメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
